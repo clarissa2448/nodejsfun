c42d3df5a63e0ef57b1a0ae9cf568e2ea495264b
Clarissa Xu <clarissa_xu24@outlook.com>
2017-11-04 22:36:20 -0400

    server start

--- app/index.js
+++ app/index.js
@@ -12,5 +12,5 @@ const result = calc.sum(numbersToAdd) //filename.function(parameter)
 
 const numbers = [2,4,1,5,4]
 
-console.log(`The result is ${result}`)
-console.log(numbers.filter(calc.isBiggerThanTwo))
+// console.log(`The result is ${result}`)
+// console.log(numbers.filter(calc.isBiggerThanTwo))
--- index.js
+++ index.js
@@ -3,3 +3,20 @@ require ('./app/index.js')
 
 const_ = require('lodash')
 // _.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });
+
+//HTTP servers
+const http = require('http') //uses http module
+const port = 3000 //binds to port 3000 to listen on
+
+const requestHandler = (request, resposne) => {
+	console.log(request.url)
+	response.end('Hello Node.js Server!')
+	}
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+	if (err){
+		return console.log('something bad happened', err)
+	}
+	console.log(`server is litening on ${port}`)
+})
