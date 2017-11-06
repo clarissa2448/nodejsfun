eb4586f58cf2846c3a9103a602003d2aa593ab6a
Clarissa Xu <clarissa_xu24@outlook.com>
2017-11-05 22:12:41 -0500

    express stuff

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ const_ = require('lodash')
 const http = require('http') //uses http module
 const port = 3000 //binds to port 3000 to listen on
 
-const requestHandler = (request, resposne) => {
-	console.log(request.url)
-	response.end('Hello Node.js Server!')
-	} //request handler invoked every time a request hits a server
+const requestHandler = (request, resposne) => { console.log(request.url)
+response.end('Hello Node.js Server!')     } //request handler invoked everytime a request hits a server 
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
@@ -20,3 +18,65 @@ server.listen(port, (err) => {
 	}
 	console.log(`server is litening on ${port}`)
 })
+
+//Express
+// const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+	response.send('Hello from Express!')
+})
+app.listen(port,(err) => {
+	if (err){
+		return console.log('something bad happened', err)
+	}
+	console.log(`server is listening on ${port}`)
+})
+app.[stuff] rather than request url stuff
+
+//Express Middlewares
+//app.use is how to define middlewares
+const express = require('express')
+const app = express()
+app.use((request, response, next) => { //next signals express to go to next middleware
+  console.log(request.headers)
+  console.log('here')
+  next()
+})
+
+app.use((request, response, next) => {
+  request.chance = Math.random()
+  next()
+})
+
+app.get('/', (request, response) => {
+  response.json({
+    chance: request.chance
+  })
+})
+
+app.listen(3000)
+
+//Express Error Handling
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+  throw new Error('oops')
+})
+
+app.use((err, request, response, next) => {
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+
+
+
+
+
+
+
+
